c14918191ddd23d0428f7e728fdcfb9abfe0ce20
grimhailer <chillguytypeperson@gmail.com>
2026-01-22 17:44:06 +0000

    Add files via upload

---
+++ FirstProject/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] users = {"Admin", "User1","User2", "User3", "User4", "User5"};
+        String[] passwords = {"Password123", "ABC123","password", "pass123", "PaSsWoRd", "QWERTY123"};
+        System.out.print("Login: ");
+        System.out.print(users[0]);
+        System.out.print(" "+ passwords[0]);
+    }
+}
